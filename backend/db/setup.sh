d97656867d16ea265b6e316e3851398486f378ed
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-04-19 10:46:31 +0200

    pre deploy

--- backend/db/setup.sh
+++ backend/db/setup.sh
@@ -1,7 +1,12 @@
-psql -f install.sql -U postgres 
-PGPASSWORD=admin psql -d mattest -f structure.sql -U admin
-PGPASSWORD=admin psql -d mattest -f data.sql -U admin
+# psql -f install.sql -U postgres 
+# PGPASSWORD=postgres psql -d mattest -f structure.sql -U admin
+# PGPASSWORD=postgres psql -d mattest -f data.sql -U admin
 # GRANT ALL PRIVILEGES ON DATABASE "mattest" TO admin;
 # GRANT ALL PRIVILEGES ON SCHEMA public TO admin;
 # GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
 # ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL PRIVILEGES ON TABLES TO admin;
+
+
+psql -f install.sql -U postgres
+PGPASSWORD=postgres psql -d example -f structure.sql -U admin
+PGPASSWORD=postgres psql -d example -f data.sql -U admin
