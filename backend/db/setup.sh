f6fb7c28f072a697fd2c2a8c087a46243a52ce0f
root <root@matreshkaflower.fvds.ru>
2023-04-19 11:57:38 +0300

    add sup user to db

--- backend/db/setup.sh
+++ backend/db/setup.sh
@@ -8,5 +8,6 @@
 
 
 psql -f install.sql -U postgres
-PGPASSWORD=postgres psql -d example -f structure.sql -U admin
-PGPASSWORD=postgres psql -d example -f data.sql -U admin
+PGPASSWORD=postgres psql -d example -f structure.sql -U postgres
+PGPASSWORD=postgres psql -d example -f data.sql -U postgres
+
