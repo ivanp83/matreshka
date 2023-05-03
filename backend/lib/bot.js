c7240d740f077807ecfd7163e50e7e12d711f77d
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-03 13:02:22 +0200

    sad

--- backend/lib/bot.js
+++ backend/lib/bot.js
@@ -85,9 +85,9 @@ bot.on('document', (ctx) => {
 bot.on('pre_checkout_query', async (ctx) => {
   try {
     await ctx.answerPreCheckoutQuery(true);
-    const orderId = ctx.update.pre_checkout_query.invoice_payload.order_id;
+    const order = ctx.update.pre_checkout_query.invoice_payload.order_id;
     const productsStoreInOrder = await pool
-      .query(`SELECT order_items  FROM orders WHERE id = $1;`, [orderId])
+      .query(`SELECT order_items  FROM orders WHERE id = $1;`, [order.orderId])
       .then((res) => res.rows[0]);
     console.log(productsStoreInOrder);
 
