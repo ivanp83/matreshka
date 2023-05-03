ea80e756f52ac861a8cc6d7442be428585d62e5d
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-03 13:30:52 +0200

    ad

--- backend/lib/bot.js
+++ backend/lib/bot.js
@@ -108,7 +108,7 @@ bot.on('pre_checkout_query', async (ctx) => {
     await pool.query(
       `UPDATE orders SET order_items='${JSON.stringify(
         orderItems,
-      )}' currency=${currency} WHERE id=$1 RETURNING order_items;`,
+      )}' SET currency=${currency} WHERE id=$1 RETURNING order_items;`,
       [orderId],
     );
 
