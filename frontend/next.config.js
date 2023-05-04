fba35026f7d40bb8c72556dca8169ba6dba8a071
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-04 11:19:32 +0200

    asd

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     appDir: true,
   },
   siteUrl: process.env.SITE_URL || "https://matryoshkaflowers.ru",
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true,
   images: {
     domains: ["localhost", "static.matryoshkaflowers.ru"],
     formats: ["image/webp"],
