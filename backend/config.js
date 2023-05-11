a387259b249179bc6f863d5314690b3beb2f61ec
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-11 10:10:00 +0200

    asd

---
+++ backend/config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  static: {
+    port: 8001,
+    transport: 'https',
+  },
+  api: {
+    port: 8000,
+    transport: 'https',
+  },
+  sandbox: {
+    timeout: 5000,
+    displayErrors: false,
+  },
+  db: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'mattest',
+    user: 'admin',
+    password: 'postgres',
+  },
+  bot: {
+    tokenDev: '5090016957:AAEUQoqz8-eFnVQEUPR55lLgpTpnLq0gsas',
+    tokenProd: '6284425839:AAEmQ123Oyq6UBjK10jBoCFiY1SvJn6zKyY',
+    adminId: 1294200727,
+  },
+};
