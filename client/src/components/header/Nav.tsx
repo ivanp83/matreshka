914a537d15125bde4c0aab32af3c2731bb9ca0fd
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-18 15:52:31 +0200

    asd

--- client/src/components/header/Nav.tsx
+++ client/src/components/header/Nav.tsx
@@ -25,13 +25,13 @@ const Nav: FC<NavProps> = ({ id }) => {
             Корзина
           </NavLink>
         </li>
-        {id == 526244481 && (
-          <li>
-            <NavLink className={setActive} to={`admin`}>
-              Admin
-            </NavLink>
-          </li>
-        )}
+
+        <li>
+          <NavLink className={setActive} to={`admin`}>
+            Admin
+          </NavLink>
+        </li>
+
         {id == 1294200727 && (
           <li>
             <NavLink className={setActive} to={`admin`}>
