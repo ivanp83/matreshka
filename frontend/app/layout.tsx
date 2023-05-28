aaf951abbd9367391405b1c4416a9986e7e957f3
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-05-28 22:44:11 +0200

    fixed ym

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import { Router } from "next/router";
 import Head from "next/head";
 import * as ga from "../lib/ga";
+import { YandexMetricaProvider } from "next-yandex-metrica";
 
 const inter = Roboto_Flex({
   weight: ["400", "500", "600", "700"],
@@ -66,46 +67,29 @@ export default function RootLayout({
           `,
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-  
-            ym(${process.env.NEXT_PUBLIC_YANDEX_METRICS}, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true,
-              ecommerce:"dataLayer"
-            });
-          `,
-          }}
-        />
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/12345678"
-              style={{ position: "absolute", left: "-9999px" }}
-              alt=""
-            />
-          </div>
-        </noscript>
       </Head>
       <StyledJsxRegistry>
-        <AppProvider>
-          <body className={inter.className}>
-            <div id="root-loader" />
-            <div id="portal" />
-            <Header />
-            <MobileNav />
-            <main>{children}</main>
-            <Footer />
-          </body>
-        </AppProvider>
+        <YandexMetricaProvider
+          tagID={Number(process.env.NEXT_PUBLIC_YANDEX_METRICS)}
+          initParameters={{
+            clickmap: true,
+            trackLinks: true,
+            webvisor: true,
+            accurateTrackBounce: true,
+            ecommerce: "dataLayer",
+          }}
+        >
+          <AppProvider>
+            <body className={inter.className}>
+              <div id="root-loader" />
+              <div id="portal" />
+              <Header />
+              <MobileNav />
+              <main>{children}</main>
+              <Footer />
+            </body>
+          </AppProvider>
+        </YandexMetricaProvider>
       </StyledJsxRegistry>
     </html>
   );
