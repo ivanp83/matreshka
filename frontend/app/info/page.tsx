3c998103a1320e60ff77e9437c8ed4417a56ea22
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-06-08 18:25:33 +0200

    foxed btn color

--- frontend/app/info/page.tsx
+++ frontend/app/info/page.tsx
@@ -43,53 +43,57 @@ export default function Page() {
     >
       <section className="info">
         <style jsx>{`
-        .info {
-          min-height: calc(100vh - var(--space-big) * 2);
-          grid-column: 1/4;
-        }
-        .wrapp {
-          grid-column: 2/3;
-          grid-row-gap: var(--space-small);
-          height: fit-content;
-        }
-        h1 {
-          grid-column: 1/4;
-          justify-self: start;
-        }
-        nav {
-          grid-column: 1/2;
-    display: grid;
-    grid-template-rows: min-content;
-    text-align: left;
-    grid-gap: 10px;
-    width: fit-content;
-    grid-template-rows: min-content min-content min-content;
-        }
-        .nav-button {
-          border: none;
-          outline: none;
-          background: transparent;
-          text-align:left;
-        }
-        .nav-button:not([disabled]) {
-          cursor: pointer;
-        }
-        .nav-button.active {
-          color: var(--main-gray);
-        }
-        .content {
-          grid-column: 2/3;
-        }
-        .par {
-          display: grid;
-          grid-row-gap: 1rem;
-        }
-        @media all and (max-width:1000px) {
-          nav, .content {
+          .info {
+            min-height: calc(100vh - var(--space-big) * 2);
             grid-column: 1/4;
-          
-        }
-      `}</style>
+          }
+          .wrapp {
+            grid-column: 2/3;
+            grid-row-gap: var(--space-small);
+            height: fit-content;
+          }
+          h1 {
+            grid-column: 1/4;
+            justify-self: start;
+          }
+          nav {
+            grid-column: 1/2;
+            display: grid;
+            grid-template-rows: min-content;
+            text-align: left;
+            grid-gap: 10px;
+            width: fit-content;
+            grid-template-rows: min-content min-content min-content;
+          }
+          .nav-button {
+            border: none;
+            outline: none;
+            background: transparent;
+            text-align: left;
+          }
+          button {
+            color: inherit;
+          }
+          .nav-button:not([disabled]) {
+            cursor: pointer;
+          }
+          .nav-button.active {
+            color: var(--main-gray);
+          }
+          .content {
+            grid-column: 2/3;
+          }
+          .par {
+            display: grid;
+            grid-row-gap: 1rem;
+          }
+          @media all and (max-width: 1000px) {
+            nav,
+            .content {
+              grid-column: 1/4;
+            }
+          }
+        `}</style>
         <div className="wrapp container">
           <nav aria-label="Навигация по клиентской информации">
             {tabs.map((tab) => (
