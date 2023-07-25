576311be4cb523172c1584cbbc9f56dd45ae981d
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-07-25 14:42:01 +0200

    asd

--- front/app/about/page.js
+++ front/app/about/page.js
@@ -2,25 +2,20 @@ import { Metadata } from "next";
 import Index from "../components/about";
 import Head from "next/head";
 
-// export const metadata = {
-//   title: "Наша история | Цветочный бутик Матрёшка",
-//   description:
-//     ",
-// };
 export default function Page() {
   return (
     <>
-      <Head itemscope itemtype="https://schema.org/WPHeader">
-        <title itemprop="name">Наша история | Цветочный бутик Матрёшка</title>
+      <Head itemScope itemType="https://schema.org/WPHeader">
+        <title itemProp="name">Наша история | Цветочный бутик Матрёшка</title>
 
         <meta
-          itemprop="description"
+          itemProp="description"
           name="description"
           content="Как хобби стало любимым делом. История создания интернет-магазина Матрёшка."
         />
 
         <meta
-          itemprop="keywords"
+          itemProp="keywords"
           name="keywords"
           content="маркетинговое агентство продвижение бизнеса интернет реклама digital agency marketing москва"
         />
