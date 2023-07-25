b554cce66cd104e21175392b233257ae72ee6c64
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-07-25 14:35:18 +0200

    asd

--- front/app/about/page.js
+++ front/app/about/page.js
@@ -1,20 +1,31 @@
+import { Metadata } from "next";
 import Index from "../components/about";
-const jsonLd = {
-  "@context": "https://schema.org",
-  "@type": "WPHeader",
-  title: "Наша история | Цветочный бутик Матрёшка",
-  description:
-    "Как хобби стало любимым делом. История создания интернет-магазина Матрёшка.",
-};
+import Head from "next/head";
 
+// export const metadata = {
+//   title: "Наша история | Цветочный бутик Матрёшка",
+//   description:
+//     ",
+// };
 export default function Page() {
   return (
     <>
+      <Head itemscope itemtype="https://schema.org/WPHeader">
+        <title itemprop="name">Наша история | Цветочный бутик Матрёшка</title>
+
+        <meta
+          itemprop="description"
+          name="description"
+          content="Как хобби стало любимым делом. История создания интернет-магазина Матрёшка."
+        />
+
+        <meta
+          itemprop="keywords"
+          name="keywords"
+          content="маркетинговое агентство продвижение бизнеса интернет реклама digital agency marketing москва"
+        />
+      </Head>
       <Index />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-      />
     </>
   );
 }
