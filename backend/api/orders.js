c90a332f6370c061889edd2ce5eb3644e01e7728
ivanp <45138902+ivanp83@users.noreply.github.com>
2023-11-28 15:03:31 +0200

    ad

--- backend/api/orders.js
+++ backend/api/orders.js
@@ -10,46 +10,53 @@ const { appEmitter } = require('../utils/EventEmitter');
 
 module.exports = {
   async read(id, isAdmin, fields = ['*']) {
-    const names = fields.join(', ');
-    const sql = `SELECT ${names} FROM orders`;
-    if (!id) return orders.queryRows(sql);
+    try {
+      const names = fields.join(', ');
+      const sql = `SELECT ${names} FROM orders`;
+      if (!id) return orders.queryRows(sql);
 
-    const order = await orders.queryRows(`${sql} WHERE orders.id = $1`, [id]);
+      const order = await orders.queryRows(`${sql} WHERE orders.id = $1`, [id]);
 
-    const myHeaders = new Headers();
-    myHeaders.append('Idempotence-Key', `${new Date().getTime()}`);
-    myHeaders.set(
-      'Authorization',
-      'Basic ' +
-        Buffer.from(
-          config.yookassa.shopId + ':' + config.yookassa.token,
-        ).toString('base64'),
-    );
-
-    myHeaders.append('Content-Type', 'application/json');
-
-    const requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-    };
-    const yookassaResponse = await fetch(
-      `${config.yookassa.uri}/${order[0].yookassa_id}`,
-      requestOptions,
-    )
-      .then((response) => response.json())
-      .then((result) => result);
-    console.log({ yookassaResponse });
-    if (!['succeeded', 'canceled'].includes(order[0].status)) {
-      order[0].status = yookassaResponse.status;
-      await orders.queryRows(
-        `UPDATE orders
-        SET order_status = $1
-        WHERE orders.id=$2;`,
-        [yookassaResponse.status, order[0].id],
+      const myHeaders = new Headers();
+      myHeaders.append('Idempotence-Key', `${new Date().getTime()}`);
+      myHeaders.set(
+        'Authorization',
+        'Basic ' +
+          Buffer.from(
+            config.yookassa.shopId + ':' + config.yookassa.token,
+          ).toString('base64'),
       );
-    }
 
-    return await orders.queryRows(`${sql} WHERE orders.id = $1`, [order[0].id]);
+      myHeaders.append('Content-Type', 'application/json');
+
+      const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+      };
+
+      const yookassaResponse = await fetch(
+        `${config.yookassa.uri}/${order[0].yookassa_id}`,
+        requestOptions,
+      )
+        .then((response) => response.json())
+        .then((result) => result);
+      console.log({ yookassaResponse });
+      if (!['succeeded', 'canceled'].includes(order[0].status)) {
+        order[0].status = yookassaResponse.status;
+        await orders.queryRows(
+          `UPDATE orders
+          SET order_status = $1
+          WHERE orders.id=$2;`,
+          [yookassaResponse.status, order[0].id],
+        );
+      }
+
+      return await orders.queryRows(`${sql} WHERE orders.id = $1`, [
+        order[0].id,
+      ]);
+    } catch (error) {
+      console.log({ error });
+    }
   },
 
   async create({ products: productsReq, userId }) {
